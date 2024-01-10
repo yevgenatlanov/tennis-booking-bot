508fcd6b7cb1d4e83724be9a9e866bbe7ad85bce
Yevgen Atlanov <yevgen.atlanv@gmail.com>
2024-01-10 05:33:04 +0300

    final fix for env

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,12 @@
 import * as admin from 'firebase-admin';
 require('dotenv').config();
 
-const serviceAccount = JSON.parse(process.env.SA);
+const serviceAccountEnv = process.env.SA;
+if (!serviceAccountEnv) {
+  throw new Error('The SA environment variable is not set.');
+}
+
+const serviceAccount = JSON.parse(serviceAccountEnv);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
